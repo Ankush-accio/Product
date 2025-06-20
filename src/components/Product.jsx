34594631c641535699aecd7c547badc876f0aa51
Ankush Rohilla <ankushrohilla@mukul-mac-book-Air.local>
2025-06-21 00:59:15 +0530

    productDetail page by params id

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,88 +1,17 @@
 import React, { useEffect, useState } from 'react'
+import {useNavigate} from "react-router-dom"
 
 
-const Poroduct = () => { 
+const Poroduct = ({data}) => { 
   useEffect(()=>{
     AllHandlerFn()
   } ,[])
+
+const navigate = useNavigate()
 const [val , setVal] = useState([])
 const [price , setPrice] = useState(500)
 
-const[data ,setData] = useState( [
-  {
-    id: 1,
-    title: "Running Shoes",
-    price: 99,
-    rating: 4.5,
-    image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cnVubmluZyUyMHNob2VzfGVufDB8fDB8fHww", 
-    category: "Clothing",
-    description: "Comfortable and lightweight running shoes."
-  },
-  {
-    id: 2,
-    title: "Wireless Headphones",
-    price: 149,
-    rating: 4.3,
-    image: "https://images.unsplash.com/photo-1487215078519-e21cc028cb29?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aGVhZHBob25lfGVufDB8fDB8fHww", 
-    category: "Electronics",
-    description: "High-quality wireless sound with long battery life."
-  },
-  {
-    id: 3,
-    title: "Backpack",
-    price: 129,
-    rating: 4.2,
-    image: "https://media.istockphoto.com/id/2153974167/photo/hiking-background-and-backback-in-nature-for-adventure-explore-and-fitness-with-equipment.webp?a=1&b=1&s=612x612&w=0&k=20&c=rxva-2rh-oSzFrRgqwTIqKjxb5uRXaG3pjrhuixqhpk=", 
-    category: "Accessories",
-    description: "Spacious and durable backpack for everyday use."
-  },
-  {
-    id: 4,
-    title: "Smartwatch",
-    price: 249,
-    rating: 4.6,
-    image: "https://images.unsplash.com/photo-1660844817855-3ecc7ef21f12?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHNtYXJ0d2F0Y2h8ZW58MHx8MHx8fDA%3D", 
-    category: "Electronics",
-    description: "Track your fitness and notifications on the go."
-  },
-  {
-    id: 5,
-    title: "Sunglasses",
-    price: 149,
-    rating: 4.1,
-    image: "https://images.unsplash.com/photo-1511499767150-a48a237f0083?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3VuZ2xhc3N8ZW58MHx8MHx8fDA%3D", 
-    category: "Accessories",
-    description: "Stylish sunglasses for sunny days."
-  },
-  {
-    id: 6,
-    title: "Digital Camera",
-    price: 499,
-    rating: 4.4,
-    image: "https://images.unsplash.com/photo-1614008247224-a9b185e63cfd?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGRpZ2l0YWwlMjBjYW1lcmF8ZW58MHx8MHx8fDA%3D", 
-    category: "Electronics",
-    description: "Capture moments in high resolution."
-  },
-  {
-    id: 7,
-    title: "T-shirt",
-    price: 29,
-    rating: 4.0,
-    image: "https://plus.unsplash.com/premium_photo-1718913936342-eaafff98834b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8VCUyMHNoaXJ0fGVufDB8fDB8fHww", 
-    category: "Clothing",
-    description: "Soft and breathable cotton T‑shirt."
-  },
-  {
-    id: 8,
-    title: "Smartphone",
-    price: 699,
-    rating: 4.7,
-    image: "https://images.unsplash.com/photo-1598965402089-897ce52e8355?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c21hcnRwaG9uZXxlbnwwfHwwfHx8MA%3D%3D", 
-    category: "Electronics",
-    description: "Latest smartphone with powerful performance."
-  }
-]
-)
+
   function AllHandlerFn (){
     let filteredData = data.filter((items)=>{
       return items
@@ -118,6 +47,11 @@ const[data ,setData] = useState( [
       return items.price <= newPrice
     })
     setVal(filteredData)
+  }
+  
+
+  function cardClickhandler(id){
+    navigate(`/product/${id}`)
   }
 
   return (
@@ -196,7 +130,9 @@ const[data ,setData] = useState( [
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
               { val.length ? val.map((items) =>{
                 return(
-                 <div key={items.id} className="max-w-[250px] bg-white rounded-lg shadow-md p-4">
+                 <div 
+                  onClick={() => cardClickhandler(items.id)}
+                 key={items.id} className="max-w-[250px] bg-white rounded-lg shadow-md p-4">
                     <img src={items.image} alt={"This is an image of " + items.title} className="w-full h-40 object-cover rounded-md"/>
                     <h1 className="mt-2 text-lg font-semibold text-gray-800">{items.title}</h1>
                     <h1 className="text-gray-600">${items.price}</h1>
---
+++ src/components/ProductDetail.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {useParams} from "react-router-dom"
+
+
+const ProductDetail = ({data}) => {
+   const {id} = useParams()
+   const product = data.find((items) => items.id == parseInt(id))
+
+  return (
+    <>
+    <div className="flex flex-col lg:flex-row items-center gap-10 p-6 max-w-6xl mx-auto mt-10">
+      
+   
+      <div className="w-full max-w-md">
+        <img src={product.image} alt={product.title} className="w-full h-auto rounded-xl shadow-lg"/>
+      </div>
+
+      <div className="w-full max-w-xl">
+        <h2 className="text-2xl mb-3">{product.title}</h2>
+        <p className="text-lg text-gray-700 mb-2">₹ {product.price}</p>
+        <p className="text-gray-600 mb-4">{product.description}</p>
+        <p className="text-sm mb-1">Category: {product.category}</p>
+        <p className="text-sm">Rating: ⭐ {product.rating}</p>
+        <button className="mt-6 px-6 py-2 bg-[#0858A9] text-white rounded-lg hover:bg-[#002D61] transition-all">
+          Add to Cart
+        </button>
+
+      </div>
+    </div>
+    <div className="max-w-[40vw] mx-auto">
+         <h4 className="text-lg font-medium mb-2">Leave a Review</h4>
+        <textarea rows="3" className="w-full p-3 border rounded-md" placeholder="Write your review here..."/>
+        <button className="mt-3 px-5 py-2 bg-[#0858A9] text-white rounded-md hover:bg-[#002D61] transition-all">Submit Review</button>
+    </div>
+    </>
+  )
+}
+
+export default ProductDetail
