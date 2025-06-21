7d6f1d55fd587f613f5449427dd2237c3adf1c88
Ankush Rohilla <ankushrohilla@mukul-mac-book-Air.local>
2025-06-21 22:21:33 +0530

    Search filtering with string matching

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Search, ShoppingCart, User} from "lucide-react";
 import { useSelector } from 'react-redux';
 import {useNavigate} from "react-router-dom"
 
 const Navbar = () => {
   const data = useSelector((store) => store.example.cart)
+  const productData = useSelector((store) => store.example.product)
   const navigate = useNavigate()
-
+  const [searchText, setSearchText] = useState("");
+  
+  const filteredProducts = productData.filter(item =>
+  item.title.toLowerCase().includes(searchText.toLowerCase())
+  );
+  console.log(filteredProducts)
   let countOfQuantity = 0
   for(let item of data){
     countOfQuantity += item.quantity
@@ -21,7 +27,25 @@ const Navbar = () => {
 
         <div className='border-white flex items-center relative '>
             <Search className='absolute left-4' />
-            <input className='w-[40vw] sm:w-[20vw] lg:w-[40vw] border rounded-xl p-3 pl-13' type="text" placeholder='Search for products...' />
+            <input onChange={(e) => setSearchText(e.target.value)} value={searchText}
+             className='w-[40vw] sm:w-[20vw] lg:w-[40vw] border rounded-xl p-3 pl-13' type="text" placeholder='Search for products...' />
+
+            {searchText && (
+              <div className="absolute top-full mt-2 w-full max-h-60 overflow-y-auto bg-white text-black rounded shadow-md z-50">
+                {filteredProducts.length === 0 ? (
+                <p className="p-4">No products found.</p>
+                ) : (filteredProducts.map((item) => 
+                (<div key={item.id} className="flex items-center gap-4 p-3 border-b hover:bg-gray-100 cursor-pointer">
+                   <img src={item.image} alt={item.title} className="w-12 h-12 object-cover rounded" />
+                    <div>
+                      <h2 className="text-sm font-medium">{item.title}</h2>
+                      <p className="text-xs text-gray-700">$ {item.price}</p>
+                  </div>
+              </div>
+                ))
+              )}
+            </div>
+              )}
         </div>
 
 
